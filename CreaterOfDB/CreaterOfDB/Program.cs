9e9e70c1304503c470544fba54ac6d6d0ec92b23
i-geeg-i <glebov.el2005@yandex.ru>
2021-12-22 18:54:08 +0500

    v 0.11
    Nothing serious, need to delete it

---
+++ CreaterOfDB/CreaterOfDB/Program.cs
@@ -0,0 +1,24 @@
+﻿using Npgsql;
+using System;
+
+namespace CreaterOfDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string username = "student";
+            string password = username;
+            var connString = $"Host = 192.168.56.101; Port = 5432; Database = Students; Username ={username}; Password ={password}";
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand("CREATE TABLE Test(id INTEGER PRIMARY KEY)", conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
